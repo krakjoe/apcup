442f6c764678b0f5fd7d8e0d02176a70ad9f5214
krakjoe <joe.watkins@live.co.uk>
2013-03-14 18:49:43 +0000

    ...

--- apcup.php
+++ apcup.php
@@ -9,15 +9,22 @@ echo "<pre>";
 if (extension_loaded("apcup")) {
     /* registers constant id, given some params */
 	var_dump(apcup_create("APCUP_DEFAULT", 2048, 7020, 7020));
+    var_dump(apcup_create("APCUP_OTHER", 2048, 7020, 7020));
+    
+	var_dump(APCUP_DEFAULT);
+	var_dump(APCUP_OTHER);
 	
 	/* test setting in specific cache */
-	var_dump(apcup_set(APCUP_DEFAULT, "test", $_SERVER));
-    var_dump(apcup_get(APCUP_DEFAULT, "test"));
-	var_dump(apcup_info(APCUP_DEFAULT));
-	var_dump(apcup_clear(APCUP_DEFAULT));
+	if ($_REQUEST["set"]) {
+	    apcup_set(APCUP_DEFAULT, "default", $_REQUEST);
+	    apcup_set(APCUP_OTHER,   "other", $_SERVER);
+	}
+
+	var_dump(apcup_get(APCUP_OTHER, "other"));
+    var_dump(apcup_get(APCUP_DEFAULT, "default"));
+	
+	var_dump(apcup_info(APCUP_OTHER));
 	var_dump(apcup_info(APCUP_DEFAULT));
 	/* test getting from specific cache */
 } else die("canot load apcup");
-
-print_r(get_extension_funcs("apcup"));
 ?>
